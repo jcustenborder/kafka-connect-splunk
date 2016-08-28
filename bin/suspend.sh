455370e57d8d7a34a723bd5d403441c37c14a489
Jeremy Custenborder <jcustenborder@gmail.com>
2016-08-27 23:41:03 -0500

    Added build logic to create and upload releases to github

--- bin/suspend.sh
+++ bin/suspend.sh
@@ -25,7 +25,7 @@ else
     exit 1
 fi
 
-export CLASSPATH="$(find `pwd`/target/kafka-connect-salesforce-1.0-SNAPSHOT-package/share/java/ -type f -name '*.jar' | tr '\n' ':')"
+export CLASSPATH="$(find target/ -type f -name '*.jar'| grep '\-package' | tr '\n' ':')"
 export KAFKA_JMX_OPTS='-Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005'
 
 $CONFLUENT_HOME/bin/connect-standalone connect/connect-avro-docker.properties config/salesforce.properties
