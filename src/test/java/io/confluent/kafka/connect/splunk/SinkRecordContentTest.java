0618a26caec2f5852075a9147d043c8a864d6460
Jeremy Custenborder <jcustenborder@gmail.com>
2017-01-19 17:33:38 -0600

    Repro of issue-3.

--- src/test/java/io/confluent/kafka/connect/splunk/SinkRecordContentTest.java
+++ src/test/java/io/confluent/kafka/connect/splunk/SinkRecordContentTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016 Jeremy Custenborder (jcustenborder@gmail.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
@@ -50,6 +51,36 @@ public class SinkRecordContentTest {
         )
     );
   }
+
+  @Test
+  public void issue3() throws IOException {
+
+    Map<?, ?> input = ImmutableMap.of("host", "hostname.example.com", "time", new Date(1472256858924L), "source", "testapp");
+
+    Collection<SinkRecord> sinkRecords = Arrays.asList(
+        new SinkRecord(
+            "topic",
+            1,
+            null,
+            null,
+            null,
+            input,
+            1L
+        )
+    );
+
+    ObjectMapper mapper = ObjectMapperFactory.create();
+
+    SinkRecordContent content = new SinkRecordContent(mapper, sinkRecords);
+    String actual;
+    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+      content.writeTo(outputStream);
+      actual = new String(outputStream.toByteArray(), "UTF-8");
+    }
+    System.out.println(actual);
+//    Assert.assertEquals(expected, actual);
+  }
+
 
   @Test
   public void writeTo() throws IOException {
